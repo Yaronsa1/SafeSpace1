c27f8f632b6d9395bdd648ed5c4f4cdbaff73363
GiladAbitbul <95180360+GiladAbitbul@users.noreply.github.com>
2021-12-27 00:01:57 +0200

    Delete base directory

--- base/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from base.models import Place,Restrictions,Comment
-# Register your models here.
-
-admin.site.register(Place)
-admin.site.register(Comment)
-admin.site.register(Restrictions)
-
-
--- base/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from base.models import Place
-
-class PlaceForm(ModelForm):
-    class Meta:
-        model = Place
-        fields = '__all__'
-        exclude = ['owner']
