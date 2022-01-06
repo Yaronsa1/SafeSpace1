e89a113d4dc7092427ce1c6456511d9aafb469b3
amitavigdor <amitav4@ac.sce.ac.il>
2022-01-06 23:09:49 +0200

    added unit test and fixed pictures in business

--- base/views.py
+++ base/views.py
@@ -17,36 +17,57 @@ from django.contrib import messages
 # Create your views here.
 
 
-def password_reset_request(request):
+def homePage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    places = Place.objects.filter(
+        Q(restrictions__name__icontains = q) |
+        Q(name__icontains = q) |
+        Q(description__icontains = q) |
+        Q(placePhoneNumber__icontains = q)).distinct()
+    placesComments = Comment.objects.filter(Q(place__restrictions__name__icontains = q)).distinct()
+    restrictions = Restrictions.objects.all()
+    context = {'places':places,'restrictions':restrictions,'placesComments':placesComments}
+    return render(request, 'base/homePage.html',context)
+
+
+@login_required (login_url= 'loginPage')
+def userProfile(request,primaryKey):
+    currentUser = User.objects.get(id = primaryKey)
+    place = currentUser.place_set.all()
+    placeComments = currentUser.comment_set.all()
+    restrictions = Restrictions.objects.all()
+    context = {'currentUser':currentUser,'places':place,'placesComments':placeComments,'restrictions':restrictions}
+    return render(request, 'base/profile.html',context)
+
+def placePage(request,primaryKey):
+    currentPlace = Place.objects.get(id = primaryKey)
+    comments = currentPlace.comment_set.all()
+    restrictions = Restrictions.objects.all()
     if request.method == "POST":
-        password_reset_form = PasswordResetForm(request.POST)
-        if password_reset_form.is_valid():
-            data = password_reset_form.cleaned_data['email']
-            associated_users = User.objects.filter(Q(email=data))
-            if associated_users.exists():
-                for user in associated_users:
-                    subject = "Password Reset Requested"
-                    email_template_name = "email.txt"
-                    c = {
-                        "email": user.email,
-                        'domain': '127.0.0.1:8000',
-                        'site_name': 'Website',
-                        "uid": urlsafe_base64_encode(force_bytes(user.pk)),
-                        "user": user,
-                        'token': default_token_generator.make_token(user),
-                        'protocol': 'http',
-                    }
-                    email = render_to_string(email_template_name, c)
-                    try:
-                        send_mail(subject, email, 'admin@example.com', [user.email], fail_silently=True)
-                    except BadHeaderError:
-                        return HttpResponse('Invalid header found.')
-                    messages.success(request, 'A message with reset password instructions has been sent to your inbox.')
-                    return redirect('/password_reset/done')
-            messages.error(request, 'An invalid email has been entered.')
-    password_reset_form = PasswordResetForm()
-    return render(request=request, template_name="password_reset.html",
-                  context={"password_reset_form": password_reset_form})
+        comment = Comment.objects.create(
+            owner = request.user,
+            place = currentPlace,
+            body = request.POST.get('body'),
+        )
+        return redirect('placePage', primaryKey = currentPlace.id)
+    context = {'place':currentPlace, 'placesComments':comments,'restrictions':restrictions}
+    return render(request, 'base/placePage.html',context)
+
+
+def contactUs(request):
+    return render(request, 'base/contactUs.html')
+
+
+#mobile
+def restrictionsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    restrictions = Restrictions.objects.filter(name__icontains=q)
+    return render(request, 'base/restrictions.html', {'restrictions':restrictions})
+
+
+def activityPage(request):
+    comments = Comment.objects.all()
+    return render(request, 'base/activity.html',{'comments':comments})
 
 def loginPage(request):
     page ='loginPage'
@@ -56,23 +77,23 @@ def loginPage(request):
         userName = request.POST.get('username') #####
         password = request.POST.get('password')
         try:
-            user = User.objects.get(username = userName) 
+            user = User.objects.get(username = userName)
         except:
-            messages.error(request, 'שם המשתמש לא קיים במערכת') 
+            messages.error(request, 'שם המשתמש לא קיים במערכת')
         user = authenticate(request, username = userName, password = password)
         if user is not None:
             login(request, user)
             return redirect('homePage')
         else:
-            messages.error(request, 'שם המשתמש או סיסמה אינם נכונים') 
+            messages.error(request, 'שם המשתמש או סיסמה אינם נכונים')
     context = {'page':page}
     return render(request, 'base/loginRegister.html',context)
-
 
 
 def logoutPage(request):
     logout(request)
     return redirect('homePage')
+
 
 def registerPage(request):
     page = 'register'
@@ -108,41 +129,6 @@ def businessOwnerRegisterPage(request):
     context={'page':page,'form':form}
     return render(request, 'base/loginRegister.html', context)
 
-def homePage(request):
-    q = request.GET.get('q') if request.GET.get('q') != None else ''
-    places = Place.objects.filter(
-        Q(restrictions__name__icontains = q) |
-        Q(name__icontains = q) |
-        Q(description__icontains = q) |
-        Q(placePhoneNumber__icontains = q)).distinct()
-    placesComments = Comment.objects.filter(Q(place__restrictions__name__icontains = q)).distinct()
-    restrictions = Restrictions.objects.all()
-    context = {'places':places,'restrictions':restrictions,'placesComments':placesComments}
-    return render(request, 'base/homePage.html',context)
-
-
-def placePage(request,primaryKey):
-    currentPlace = Place.objects.get(id = primaryKey)
-    comments = currentPlace.comment_set.all()
-    restrictions = Restrictions.objects.all()
-    if request.method == "POST":
-        comment = Comment.objects.create(
-            owner = request.user,
-            place = currentPlace,
-            body = request.POST.get('body'),
-        )
-        return redirect('placePage', primaryKey = currentPlace.id)
-    context = {'place':currentPlace, 'placesComments':comments,'restrictions':restrictions}
-    return render(request, 'base/placePage.html',context)
-
-@login_required (login_url= 'loginPage')
-def userProfile(request,primaryKey):
-    currentUser = User.objects.get(id = primaryKey)
-    place = currentUser.place_set.all()
-    placeComments = currentUser.comment_set.all()
-    restrictions = Restrictions.objects.all()
-    context = {'currentUser':currentUser,'places':place,'placesComments':placeComments,'restrictions':restrictions}
-    return render(request, 'base/profile.html',context)
 
 @login_required (login_url= 'loginPage')
 def createPlace(request):
@@ -200,6 +186,7 @@ def commentDelete(request,primaryKey):
         return redirect('homePage')
     return render(request, 'base/commentDelete.html', {'obj':currentComment})
 
+
 @login_required(login_url= 'loginPage')
 def updateUser(request):
     user = request.user
@@ -212,15 +199,35 @@ def updateUser(request):
             return redirect('userProfile', primaryKey=user.id)
     return render(request,'base/updateUser.html',{'form':form})
 
-def contactUs(request):
-    return render(request, 'base/contactUs.html')
+def password_reset_request(request):
+    if request.method == "POST":
+        password_reset_form = PasswordResetForm(request.POST)
+        if password_reset_form.is_valid():
+            data = password_reset_form.cleaned_data['email']
+            associated_users = User.objects.filter(Q(email=data))
+            if associated_users.exists():
+                for user in associated_users:
+                    subject = "Password Reset Requested"
+                    email_template_name = "email.txt"
+                    c = {
+                        "email": user.email,
+                        'domain': '127.0.0.1:8000',
+                        'site_name': 'Website',
+                        "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                        "user": user,
+                        'token': default_token_generator.make_token(user),
+                        'protocol': 'http',
+                    }
+                    email = render_to_string(email_template_name, c)
+                    try:
+                        send_mail(subject, email, 'admin@example.com', [user.email], fail_silently=True)
+                    except BadHeaderError:
+                        return HttpResponse('Invalid header found.')
+                    messages.success(request, 'A message with reset password instructions has been sent to your inbox.')
+                    return redirect('/password_reset/done')
+            messages.error(request, 'An invalid email has been entered.')
+    password_reset_form = PasswordResetForm()
+    return render(request=request, template_name="password_reset.html",
+                  context={"password_reset_form": password_reset_form})
 
-#mobile
-def restrictionsPage(request):
-    q = request.GET.get('q') if request.GET.get('q') != None else ''
-    restrictions = Restrictions.objects.filter(name__icontains=q)
-    return render(request, 'base/restrictions.html', {'restrictions':restrictions})
 
-def activityPage(request):
-    comments = Comment.objects.all()
-    return render(request, 'base/activity.html',{'comments':comments})
